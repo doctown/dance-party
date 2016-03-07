6475b2b8c93d64b8d3e6d2ac24703e86b0150f91
David Ogor <doctown@yahoo.com>
2016-03-07 14:22:49 -0800

    Add new dancers but code not individualized yet

---
+++ src/outOfTimeDancer.js
@@ -0,0 +1,21 @@
+var makeOutOfTimeDancer = function(top, left, timeBetweenSteps) {
+  dancer.call(this, top, left, timeBetweenSteps);
+
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+  var oldStep = step;
+
+  this.step = function() {
+    // call the old version of step at the beginning of any call to this new version of step
+    oldStep();
+    // toggle() is a jQuery method to show/hide the <span> tag.
+    // See http://api.jquery.com/category/effects/ for this and
+    // other effects you can use on a jQuery-wrapped html tag.
+    this.$node.toggle();
+  };
+  return this;
+};
+
+makeOutOfTimeDancer.prototype = Object.create(dancer);
+makeOutOfTimeDancer.prototype.constructor = makeOutOfTimeDancer;

7d3089bacdb0ba7637c372fe14bf42cfbbdfa119
David Ogor <doctown@yahoo.com>
2016-03-07 20:56:34 -0800

    Change the dancer to pseudoclassical

--- src/dancer.js
+++ src/dancer.js
@@ -1,28 +1,33 @@
 // Creates and returns a new dancer object that can step
 var dancer = function(top, left, timeBetweenSteps) {
+  this.top = top;
+  this.left = left;
+  this.timeBetweenSteps = timeBetweenSteps;
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
 
-  this.step = function() {
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(this.step, timeBetweenSteps);
-  };
   this.step();
-
-  this.setPosition = function(top, left) {
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    this.$node.css(styleSettings);
-  };
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  this.setPosition(top, left);
+  this.setPosition(this.top, this.left);
+};
+
+dancer.prototype.step = function() {
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  var step = this.step.bind(this);
+  setTimeout(step, this.timeBetweenSteps);
+};
+
+dancer.prototype.setPosition = function(top, left) {
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);
 };
